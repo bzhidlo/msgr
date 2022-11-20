f4e9a9b881407eccdf276f588aa6ca801d18416d
bzhidlo <zhukovaaa98@gmail.com>
2022-11-20 13:55:44 +0300

    studiyng fastapi

--- server/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("tutorial.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE messages(dialog_id, content, update_time)")
-res = cur.execute("SELECT name FROM sqlite_master")
-res.fetchone()
