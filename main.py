0a138bcbcddc42fc76bfba9df1b56f9b4741925c
bzhidlo <zhukovaaa98@gmail.com>
2022-12-06 11:51:56 +0300

    resolving

--- main.py
+++ main.py
@@ -28,86 +28,9 @@ class ConnectionManager:
 manager = ConnectionManager()
 
 
-<<<<<<< HEAD
-#пример запросов query параметры
-from typing import Union
-
-@app.get("/items/{item_id}")
-async def read_user_item(
-    item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None
-):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
-
-
-#запрос с телом запроса
-from pydantic import BaseModel
-
-class Message(BaseModel):
-    from_id: str
-    to_id: str
-    message: str
-    time: int
-    changed: Union[bool, None] = None
-
-@app.post("/chat/")
-async def create_message(item: Message):
-    chat = []
-    chat.append(item) 
-    return chat
-
-
-from fastapi import Query
-
-@app.get("/query/")
-async def read_items(q: Union[str, None] = Query(default=None, max_length=50)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
-
-
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
-
-class UserBase(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-class UserIn(UserBase):
-    password: str
-
-
-class UserOut(UserBase):
-    pass
-
-
-class UserInDB(UserBase):
-    hashed_password: str
-
-
-def fake_password_hasher(raw_password: str):
-    return "supersecret" + raw_password
-
-
-def fake_save_user(user_in: UserIn):
-    hashed_password = fake_password_hasher(user_in.password)
-    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
-    print("User saved! ..not really")
-    return user_in_db
-
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user_in: UserIn):
-    user_saved = fake_save_user(user_in)
-    return user_saved
-=======
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     while True:
         data = await websocket.receive_text()
         await manager.broadcast(f"Client {client_id}: {data}")
->>>>>>> 2d9a2934f22173d97c045bc6172e5f671148a314
