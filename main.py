2d9a2934f22173d97c045bc6172e5f671148a314
bzhidlo <zhukovaaa98@gmail.com>
2022-12-06 09:11:00 +0300

    group chat

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
+from typing import List
 from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-from database.db_connect import db_connect
-from fastapi.staticfiles import StaticFiles
-
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
-db_connect()
+html = ""
+with open('index.html', 'r') as f:
+    html = f.read()
 
 @app.get("/")
 async def get():
-    return HTMLResponse(StaticFiles())
+    return HTMLResponse(html)
+    
+class ConnectionManager:
+    def __init__(self):
+        self.connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.connections:
+            await connection.send_text(data)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        await manager.broadcast(f"Client {client_id}: {data}")
